9df469654b4f4a53f9eae1ba01a1b627060d9e3f
Harshal Benake <harshalbenake@Amruts-Mac-mini.local>
2015-05-05 18:00:17 +0530

    Initial Commit

---
+++ ReadPDF/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReadPDF
+//
+//  Created by Harshal Benake on 05/05/15.
+//  Copyright (c) 2015 Harshal Benake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReadPDF/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReadPDF
+//
+//  Created by Harshal Benake on 05/05/15.
+//  Copyright (c) 2015 Harshal Benake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
